28e68a593b9e4f7f5111c46d13fa6980786f6efe
douglas-emeefe <douglasoliveira.main@gmail.com>
2023-10-15 20:58:45 -0400

    First Commit Test VScode

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    printf("Criado por: Douglas Oliveira\n\n");
+    int a = 10, b = 20, soma;
+
+    soma = a + b;
+
+    printf("%d + %d = %d\n", a, b, soma);
+
+    return 0;
+}
